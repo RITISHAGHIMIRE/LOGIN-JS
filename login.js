dc33b11dbe6d81eb1ccc7184837908425017e81c
Ritisha <ritishaghimire580@gmail.com>
2023-12-26 09:38:25 +0545

    first commit

---
+++ login.js
@@ -0,0 +1,14 @@
+let ab=document.getElementById('box')
+function login(){
+   if (ab.style.transform==="translateY(1%)"){
+    ab.style.transform="translateY(-1000%)"
+   }
+   else{
+    ab.style.transform="translateY(1%)"
+   }
+}
+
+let bc=document.getElementById('bo')
+function register(){
+    bc.style.transform="translateY(1%)"
+}
